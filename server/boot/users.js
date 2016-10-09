8ee08ad0fe56b9606239423f1d270348d52d4640
Krzysztof Kunowski <kkunowski@yahoo.com>
2016-10-09 17:31:53 +0200

    cleanup

--- server/boot/users.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app) {
-        console.log("setting default users");
-        var User = app.models.User;
-		 User.find({"username":"admin"}, function(err, info) {
-            if (info.length == 0 || info.role === undefined) { User.create({"username":"admin","password":"admin","email":"admin@admin.com","role":"admin"}, function(err, info) { }); }
-			if (err != null) { console.log("error = " + err); }
-        });
-};
